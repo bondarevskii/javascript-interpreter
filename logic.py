24dfa137e11417d87498fa20562bc385fcc7a09b
Бондаревский Алексей <bondarevskii.as@gmail.com>
2020-03-25 16:40:48 +1000

    create class logic

--- logic.py
+++ logic.py
@@ -1,26 +1,31 @@
-import sys
 from antlr4 import *
 from ANTLR.JavaScriptLexer import JavaScriptLexer
 from ANTLR.JavaScriptParser import JavaScriptParser
 
 
-def main(argv):
-    input_stream = FileStream(argv[1])
-    lexer = JavaScriptLexer(input_stream)
-    for token in lexer.getAllTokens():
-        if token.type != 107:
-            print('token.text = "' + token.text + '" token.type = "' + str(token.type) + '"')
-        else:
-            print('token.text = "' + "\\n" + '" token.type = "' + str(token.type) + '"')
-    # Create new lexer because method getAllTokens() delete all tokens
-    input_stream = FileStream(argv[1])
-    lexer = JavaScriptLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = JavaScriptParser(stream)
-    tree = parser.program()
-    print(tree.toStringTree(parser.ruleNames))
+class Logic:
 
+    def __init__(self, file):
+        self.file = file
 
-if __name__ == '__main__':
-    sys.argv.append("C:/gitprojects/javascript-interpreter/code.js")
-    main(sys.argv)
+    def getLexer(self):
+        input_stream = FileStream(self.file)
+        lexer = JavaScriptLexer(input_stream)
+        return lexer
+
+    def getTokens(self, lexer):
+        result = lexer.getAllTokens()
+        return result
+
+    def getParser(self, lexer):
+        stream = CommonTokenStream(lexer)
+        parser = JavaScriptParser(stream)
+        return parser
+
+    def getTree(self, parser):
+        tree = parser.program()
+        return tree
+
+    def toStringTree(self, parser, tree):
+        string = tree.toStringTree(parser.ruleNames)
+        return string
