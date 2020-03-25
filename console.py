85736bc7320e9e2737cf19151779ac67e79d2fea
Бондаревский Алексей <bondarevskii.as@gmail.com>
2020-03-25 12:58:48 +1000

    add print tokens and tree

--- console.py
+++ console.py
@@ -7,11 +7,20 @@ from ANTLR.JavaScriptParser import JavaScriptParser
 def main(argv):
     input_stream = FileStream(argv[1])
     lexer = JavaScriptLexer(input_stream)
+    for token in lexer.getAllTokens():
+        if token.type != 107:
+            print('token.text = "' + token.text + '" token.type = "' + str(token.type) + '"')
+        else:
+            print('token.text = "' + "\\n" + '" token.type = "' + str(token.type) + '"')
+    # Create new lexer because method getAllTokens() delete all tokens
+    input_stream = FileStream(argv[1])
+    lexer = JavaScriptLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = JavaScriptParser(stream)
-    tree = parser.startRule()
-    print(tree)
+    tree = parser.program()
+    print(tree.toStringTree(parser.ruleNames))
 
 
 if __name__ == '__main__':
+    sys.argv.append("C:/gitprojects/javascript-interpreter/code.js")
     main(sys.argv)
