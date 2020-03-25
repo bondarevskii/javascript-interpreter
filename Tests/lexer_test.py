48bc40c27efcec749a08e80154f9ef9c28bf32e4
Бондаревский Алексей <bondarevskii.as@gmail.com>
2020-03-25 17:01:18 +1000

    add test for lexer

---
+++ Tests/lexer_test.py
@@ -0,0 +1,15 @@
+from logic import Logic
+
+
+def test_lexer():
+    file = "C:/gitprojects/javascript-interpreter/Tests/source/lexer-test-1.js"
+    logic = Logic(file)
+    excepted_result = ["var", " ", "a", " ", "=", " ", "0", ";", "\r", "\n",
+                       "a", " ", "=", " ", "a", " ", "+", " ", "1", ";"]
+    lexer = logic.getLexer()
+    tokens = logic.getTokens(lexer)
+    result = True
+    for i in range(len(tokens)):
+        if tokens[i].text != excepted_result[i]:
+            result = False
+    assert result
